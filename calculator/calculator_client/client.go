951e53e7c22eb1c0ae8ce22dc971dcc9ed98cabd
thisisommore <ommore501@gmail.com>
2021-07-01 04:07:39 +0000

    Calculator: Update package names

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -1,4 +1,4 @@
-package client
+package calculator_client
 
 import (
 	pb "calculator/calculatorpb"
--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -1,4 +1,4 @@
-package server
+package calculator_server
 
 import (
 	pb "calculator/calculatorpb"
